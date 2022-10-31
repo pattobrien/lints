bdd623727b54953f3d3b42db65d7846f70e0c006
pattobrien <12474318+pattobrien@users.noreply.github.com>
2022-10-31 00:34:03 -0400

    feat: First commit, design_system_lints.

---
+++ packages/design_system_lints/example/design_system_lints_example.dart
@@ -0,0 +1,5 @@
+import 'package:design_system_lints/design_system_lints.dart';
+
+void main() {
+  //
+}
---
+++ packages/design_system_lints/test/design_system_lints_test.dart
@@ -0,0 +1,6 @@
+import 'package:design_system_lints/design_system_lints.dart';
+import 'package:test/test.dart';
+
+void main() {
+  //
+}
